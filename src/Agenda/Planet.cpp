c00cf08b590e6daaf5e48c704c6fdac8e9fa67a3
Daniel Souza de Campos <danielcampos14@hotmail.com.br>
2019-11-02 17:54:20 -0300

    Create Planet.cpp

---
+++ src/Agenda/Planet.cpp
@@ -0,0 +1,26 @@
+#include "Planet.hpp"
+
+namespace Agenda{
+	
+	Planet::Planet(std::string new_name, int new_visit_time){
+		this->name = new_name;
+		this->visit_time = new_visit_time;
+	}
+	
+	int Planet::getVisitTime(){
+		return this->visit_time;
+	}
+
+	void Planet::setVisitTime(int new_visit_time){
+		this->visit_time = new_visit_time;
+	}
+	
+	std::string Planet::getName(){
+		return this->name;
+	}
+
+	void Planet::setName(std::string new_name){
+		this->name = new_name;
+	}
+	
+}
