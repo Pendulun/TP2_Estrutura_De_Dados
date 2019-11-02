0723b0d47971a9dc2175c0608221adc4daa5527d
Daniel Souza de Campos <danielcampos14@hotmail.com.br>
2019-11-02 18:53:04 -0300

    Create Lectures.hpp

---
+++ include/Agenda/Lectures.hpp
@@ -0,0 +1,20 @@
+#ifndef LECTURES_H
+#define LECTURES_H
+#include <vector>
+#include <string>
+#include "Planet.hpp"
+namespace Agenda{
+	class Lectures{
+	public:
+		void addPlanet(std::string planet_name, int t);
+		void printOrderedPlanets();
+		Lectures();
+	private:
+		std::vector<Planet> visiting_planets; 
+		void orderByTime();
+		void orderByName();
+		void printInterval();
+	};
+}
+
+#endif
