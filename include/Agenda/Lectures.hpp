a0383b8be69a150d6351f355c8abae5be247bcd5
Daniel Souza de Campos <danielcampos14@hotmail.com.br>
2019-11-09 19:53:41 -0300

    Add of atribute planetNameSize

--- include/Agenda/Lectures.hpp
+++ include/Agenda/Lectures.hpp
@@ -4,16 +4,18 @@
 #include <string>
 #include "Planet.hpp"
 #include "MergeSort.hpp"
+#include "RadixSort.hpp"
 namespace Agenda{
 	class Lectures{
 	public:
 		void addSinglePlanet(std::string planet_name, int t,int position);
 		void addPlanets();
 		void printOrderedPlanets(int maxTime);
-		Lectures(int numPlanets);
+		Lectures(int numPlanets,int planetNameSize);
 		~Lectures();
 	private:
 		int numPlanets; 
+		int planetNameSize;
 		Planet* visiting_planets[numPlanets];
 		void orderByTime();
 		void orderByName(int left_index,int right_index);
